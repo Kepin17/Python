e7dd16c719656ec710fe492e4803b13cdc5376a6
Kevien.OJ <69498060+Kepin17@users.noreply.github.com>
2021-10-09 13:35:54 +0700

    Delete Phyton directory

--- Phyton/06.template/Latihan.py/latihan2.py
+++
@@ -1,7 +0,0 @@
-print("\nCONVERTED FAHRENHEIT TO KELVIN\n")
-
-fahrenheit = float(input('Masukan Suhu: '))
-print('Suhu ruangan yang kamu masukan ',fahrenheit , '°F')
-
-kelvin = (fahrenheit - 32) * 5/9 +273 
-print('Suhu dalam Kelvin adalah ',kelvin , '°K')
