4f5dc1ad2893840dcb0fa1d291329c1ddeb4ef4b
Kevien.OJ <69498060+Kepin17@users.noreply.github.com>
2021-10-13 19:41:06 +0700

    Add files via upload

---
+++ Luas lingkaran/main.py
@@ -0,0 +1,7 @@
+phi = 3.14 
+diameter = float(input('Masukan diameter : '))
+r = diameter / 2 
+luas = phi * (r**2)
+print('hasil luas lingkaran adalah ' + str(luas) , 'cmˆ3' )
+
+
---
+++ luas peregi panjang/main.py
@@ -0,0 +1,14 @@
+x = 'Luas Persegi Panjang'
+print(x)
+
+p = float(input('masukan panjang : '))
+l = float(input('masukan lebar : '))
+t = float(input('masukan tinggi : '))
+
+print('panjang = ',p ,'cm')
+print('lebar = ',l ,'cm')
+print('tinggi = ',t ,'cm')
+
+
+luas_persegi_panjang = p * l * t
+print('Hasil dari luas lingkaran tersebut adalah ',float(luas_persegi_panjang) , 'cm^2')
